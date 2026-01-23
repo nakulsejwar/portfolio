b9fcd489d8d4351f10a576b37b179d3b42cbfb2d
nakulsejwar <nakulsejwar02@gmail.com>
2026-01-23 17:27:31 +0530

    update

--- assets/js/nakul.js
+++ assets/js/nakul.js
@@ -23,3 +23,25 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+// Add this to your nakul.js
+document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll(".service-card-3d, .about-img-holder, .header-title");
+
+    cards.forEach(card => {
+        card.addEventListener("mousemove", (e) => {
+            let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+            let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+            card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+        });
+
+        card.addEventListener("mouseenter", () => {
+            card.style.transition = "none";
+        });
+
+        card.addEventListener("mouseleave", () => {
+            card.style.transition = "all 0.5s ease";
+            card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+        });
+    });
+});
